a43e225e4012d5d5965482fbcb3f5fc54e926222
Walescko <walescko@gmail.com>
2023-03-24 16:36:13 -0300

    html/css/js - basic calculus

--- javascript/js/recruitment.js
+++ javascript/js/recruitment.js
@@ -1,18 +1,19 @@
 function outRecruiter(){
-    let inName = document.getElementsByClassName("inName");
-    let inSurname = document.getElementsByClassName("inSurname");
-    let inStudyArea = document.getElementsByClassName("inStudyArea");
+    let inName = document.getElementById("inName");
+    let inSurname = document.getElementById("inSurname");
+    let inStudyArea = document.getElementById("inStudyArea");
 // let inYearOfBirth = document.getElementsById("inYearOfBirth");
     // let date = new Date();
     // const actualYear = date.getFullYear();
-    let name = inName.value;
-    let surname = inSurname.value;
-    let studyArea = inStudyArea.value;
+    // let name = inName.value;
+    // let surname = inSurname.value;
+    // let studyArea = inStudyArea.value;
     // let yearOfBirth = inYearOfBirth;
     // let age = actualYear - yearOfBirth;
     console.log("Hello World")
-    console.log("Nome: " + name + " " + surname)
-    console.log("Área de Estudo: " + studyArea)
+    // console.log("Nome: " + name + " " + surname)
+    // console.log("Área de Estudo: " + studyArea)
     // console.log("Idade: " + age);
+
 
 }
