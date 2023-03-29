59c8d35f06173c448d9464fc37eb1d46fcf8c73f
walescko <walescko@gmail.com>
2023-03-29 13:10:18 -0300

    js if complete

---
+++ javascript/js/ifSpeed.js
@@ -0,0 +1,26 @@
+let inCarName1 = document.getElementById("inCarName1");
+let inCarSpeed1 = document.getElementById("inCarSpeed1");
+let inCarName2 = document.getElementById("inCarName2");
+let inCarSpeed2 = document.getElementById("inCarSpeed2");
+
+function speed(){
+    alert("Helo World");
+    const carName1 = inCarName1.value;
+    const carName2 = inCarName2.value;
+    const carSpeed1 = Number(inCarSpeed1.value);
+    const carSpeed2 = Number(inCarSpeed2.value);
+    console.log("Carro 1: " + carName1 + " - Velocidade = " + carSpeed1 + "km/h");
+    console.log("Carro 1: " + carName2 + " - Velocidade = " + carSpeed2 + "km/h");
+    if (carSpeed1 === carSpeed2) {
+        console.log("Os carros tem a mesma velocidade");
+    }
+    if (carSpeed2 < carSpeed1){
+        console.log("O carro 1 " + carName1 + " é o mais rápido.");
+    }
+    if (carSpeed2 > carSpeed1){
+        console.log("O carro 2 " + carName2 + " é o ais rápido.");
+    }
+
+}
+let btResult = document.getElementById("btResult");
+btResult.addEventListener("click", speed);
