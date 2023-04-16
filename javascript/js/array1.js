517511a8cc21136aa2bd32b7fd032f030ad26d79
Walescko <walescko@gmail.com>
2023-04-16 19:30:21 -0300

    js - arrays 1

---
+++ javascript/js/array1.js
@@ -0,0 +1,10 @@
+const listShop = []
+console.log(listShop.length)
+listShop[0] = "arroz"
+listShop[1] = "feijão"
+listShop[2] = 55
+listShop[3] = false
+listShop[4] = "Açúcar"
+listShop[7] = "Leite"
+console.log(listShop.length)
+console.log(listShop)
