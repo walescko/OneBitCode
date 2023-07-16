f974a357e0d8f54b5d0547a1c215bb28ef34603c
Walescko Chimendes <walescko@Walescko.WSC>
2023-07-16 20:19:30 -0300

    dom - add functions

---
+++ dom/js/lineup.js
@@ -0,0 +1,29 @@
+function addAthlete(){
+    const position = document.getElementById('position').value
+    const name = document.getElementById('name').value
+    const number = document.getElementById('number').value
+
+    const confirmation = confirm("Escalar " + name + " como " + position + "?")
+    if (confirmation){
+        const teamList = document.getElementById('teamList')
+        const playerItem = document.createElement('li')
+        playerItem.id = 'player-' + number
+        playerItem.innerText = position + ": " + name + " (" + number + ")"
+        teamList.appendChild(playerItem)
+        document.getElementById('position').value = ''
+        document.getElementById('name').value = ''
+        document.getElementById('number').value = ''
+    }
+}
+function removeAthlete(){
+    const number = document.getElementById('numberToRemove').value
+    const playerToRemove = document.getElementById('player-'+number)
+
+    const confirmation = confirm('Remover o jogador ' + playerToRemove.innerText +'?')
+    if (confirmation){
+        document.getElementById('teamList').removeChild(playerToRemove) //uma alternativa usando elemento pai
+        // playerToRemove.remove() //direto no elemento
+        document.getElementById('numberToRemove').value = ''
+    }
+
+}
