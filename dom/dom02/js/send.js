07e5b6ba50900247041119cb733b026523479892
walescko <walescko@gmail.com>
2023-09-07 22:31:39 -0300

    events html dom js

---
+++ dom/dom02/js/send.js
@@ -0,0 +1,13 @@
+function register(element){
+    const username = element.children.username.value
+    const password = element.children.password.value
+    const passwordConfirmation = element.children.passwordConfirmation.value
+
+    if (password === passwordConfirmation){
+        alert("Usuário " + username + " registrado!")
+    } else {
+        alert("A Senhas não são iguais!")
+    }
+
+
+}
--- javascript/js/js/roboticTable.js
+++ javascript/js/js/roboticTable.js
@@ -1,12 +1,13 @@
 function tabuada(){
+
     let inNumber = document.getElementById("inNumber")
     let number = Number(inNumber.value)
     let table = ""
+
     for (let i = 1; i <= 20; i++){
-
         table += number + " * " + i + " = " + (number*i) + "\n"
-
     }
+
     console.log(table)
     alert("Tabuada do Número " + number +"\n\n" + table)
 
