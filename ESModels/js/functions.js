4d05f99ebaccaabbf0edb31c17cda7b7f549b471
walescko <walescko@gmail.com>
2023-09-24 15:45:41 -0300

    ES Modules

---
+++ ESModels/js/functions.js
@@ -0,0 +1,12 @@
+export function label(attributes){
+    const element = document.createElement('label')
+    Object.assign(element, attributes)
+    return element
+}
+
+export function input(attributes){
+    const element = document.createElement('input')
+    Object.assign(element, attributes)
+    return element
+}
+
