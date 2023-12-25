941b147573f731ca61757c5b82c07adcb1fe30d3
walescko <walescko@gmail.com>
2023-12-25 20:17:30 -0300

    class easy

--- POO/associationClass/index.js
+++ POO/associationClass/index.js
@@ -1,9 +1,9 @@
-// const Address = require("./Address")
+const Address = require("./Address")
 const Person = require("./Person")
 
-// const addr = new Address("rua dos bobos", 0, "Centro", "Toquinho", "SP")
-const people = new Person("João Nada", "rua dos bobos", 0, "Centro", "Toquinho", "SP")
+const addr = new Address("rua dos bobos", 0, "Centro", "Toquinho", "SP")
+const people = new Person("João Nada", addr)
 
 console.log(people)
-// console.log(people.address.fullAddress())
+console.log(people.address.fullAddress())
 console.log(people)
