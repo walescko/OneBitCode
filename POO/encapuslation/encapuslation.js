5d05302d3a718a6b8c4a2bda8e944eeed69dcd62
walescko <walescko@gmail.com>
2024-03-19 05:52:30 -0300

    js OO - encapsulation

--- POO/encapuslation/encapuslation.js
+++ POO/encapuslation/encapuslation.js
@@ -29,8 +29,8 @@ class Account {
   const userAccount = new Account(user)
   
   console.log(userAccount)
-  console.log(userAccount.#password) // Gera erro
-  console.log(userAccount.#balance) // Gera erro
-  console.log(userAccount.#authenticate()) // Gera erro
+  //console.log(userAccount.#password) // Gera erro
+  //console.log(userAccount.#balance) // Gera erro
+  //console.log(userAccount.#authenticate()) // Gera erro
   console.log(userAccount.getBalance("user@email.com", "123456"))
   console.log(userAccount.getBalance("user@email.com", "000000"))
